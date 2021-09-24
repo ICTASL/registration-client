c0da6ffc770b66c2d2793b11ff08d27f7dbd3281
Rajith <pemabandu@gmail.com>
2021-09-24 16:00:45 +0530

    remove console

--- registration/configure.sh
+++ registration/configure.sh
@@ -93,7 +93,6 @@ cp /registration-client/target/lib/* /var/www/html/registration-client/${client_
 cp /registration-client/target/MANIFEST.MF /var/www/html/registration-client/${client_version_env}/
 cp /build_files/maven-metadata.xml /var/www/html/registration-client/
 cp reg-client.zip /var/www/html/registration-client/${client_version_env}/
-echo "---6---"
 cp /registration-test-utility.zip /var/www/html/registration-test/${client_version_env}/
 
 echo "setting up nginx static content - completed"
